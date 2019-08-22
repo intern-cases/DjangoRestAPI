ad61bd3b3b1895f425263a236e3991084d063f30
Ömer Aslandoğdu <omeraslandogdu@gmail.com>
2019-08-22 17:28:38 +0300

    registiration view

--- DjangoRestAPI/accounts/api/serializers.py
+++ DjangoRestAPI/accounts/api/serializers.py
@@ -16,17 +16,17 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def save(self):
         user = User(
             email=self.validated_data['email'],
-            username=self.validated_data['password'],
+            username=self.validated_data['username'],
+            password=self.validated_data['password'],
             is_company=self.validated_data['is_company'],
             is_dealer=self.validated_data['is_dealer'],
             is_customer=self.validated_data['is_customer']
         )
-        password = self.validated_data['password']
+
         password2 = self.validated_data['password2']
-        if password != password2:
+        if user.password != password2:
             return serializers.ValidationError({'Password must match!'})
 
-        user.set_password(password)
         user.save()
         return user
 
